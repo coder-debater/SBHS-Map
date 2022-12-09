22631225b8ec232b431e6d3802feb51c590d41f5
wensenfriendandextra <wensen.dong@gmail.com>
2022-12-09 11:25:09 +1100

    feat: settings + sw.js logging

--- register-sw.js
+++ register-sw.js
@@ -1,3 +1,4 @@
+localStorage.debug = "on";
 window.addEventListener("load", () => {
     if ("serviceWorker" in navigator) {
       navigator.serviceWorker.register("sw.js");
--- sw.js
+++ sw.js
@@ -1,13 +1,56 @@
-const cacheName = 'offline';
-const cacheFiles = [
-    '/index.html',
-    '/app.webmanifest',
-    '/icon.svg',
-    '/main.css',
-    '/register-sw.js',
-    '/sw.js'
+const OFFLINE_CACHE = "offline-1";
+const DATA_CACHE = "data";
+const FILES = [
+    "/index.html",
+    "/app.webmanifest",
+    "/icon.svg",
+    "/main.css",
+    "/register-sw.js",
+    "/sw.js"
 ];
 
-self.addEventListener('install', evt => {
-  console.log('[Service Worker] Install', evt);
+function log(...args) {
+    if (!localStorage.debug) {
+        localStorage.debug = "off";
+    }
+    if (localStorage.debug === "on") {
+        console.log("[sw.js]", ...args)
+    }
+}
+
+function prettyLog(msg, ...args) {
+    if (!localStorage.debug) {
+        localStorage.debug = "off";
+    }
+    if (localStorage.debug === "on") {
+        console.log("[sw.js] "+msg, ...args)
+    }
+}
+
+self.addEventListener("install", evt => {
+    log("Registered");
+    evt.waitUntil((async () => {
+        const cache = await caches.open(OFFLINE_CACHE);
+        log("Caching files");
+        await cache.addAll(FILES);
+    })());
+});
+
+self.addEventListener("fetch", evt => {
+    evt.respondWith((async () => {
+        try {
+            const preload = await evt.preloadResponse;
+            if (preload) {
+                return preload;
+            }
+            return (await fetch(evt.request));
+        }
+        catch (err) {
+            log("Fetch failed, returning cache");
+
+            const cache = await caches.open(CACHE_NAME);
+            const cachedResponse = await cache.match(OFFLINE_URL);
+            return cachedResponse;
+        }
+    })());
 });
