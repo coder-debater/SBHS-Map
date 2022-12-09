273b26afe1d3257076030419664dd4a8db9ffaf4
wensenfriendandextra <wensen.dong@gmail.com>
2022-12-09 11:08:01 +1100

    refactor: rename manifest, split script, add back other files

--- sw.js
+++ sw.js
@@ -1,48 +1,13 @@
-/*
-Copyright 2015, 2019, 2020, 2021 Google LLC. All Rights Reserved.
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-*/
+const cacheName = 'offline';
+const cacheFiles = [
+    '/index.html',
+    '/app.webmanifest',
+    '/icon.svg',
+    '/main.css',
+    '/register-sw.js',
+    '/sw.js'
+];
 
-// eslint-disable-next-line no-unused-vars
-const OFFLINE_VERSION = 1;
-
-const CACHE_NAME = "offline";
-const OFFLINE_URL = "offline.html";
-
-self.addEventListener("install", (event) => {event.waitUntil((async () => {
-    const cache = await caches.open(CACHE_NAME);
-    await cache.add(new Request(OFFLINE_URL, { cache: "reload" }));
-})()); self.skipWaiting();});
-  
-self.addEventListener("activate", (event) => {event.waitUntil((async () => {
-    if ("navigationPreload" in self.registration) {
-        await self.registration.navigationPreload.enable();
-    }
-})()); self.clients.claim();});
-
-self.addEventListener("fetch", (event) => {if (event.request.mode === "navigate") {
-    event.respondWith((async () => {
-        try {
-            const preload = await event.preloadResponse;
-            if (preload) {
-                return preload;
-            }
-            return (await fetch(event.request));
-        }
-        catch (err) {
-            console.log("Fetch failed; returning offline page instead.", err);
-
-            const cache = await caches.open(CACHE_NAME);
-            const cachedResponse = await cache.match(OFFLINE_URL);
-            return cachedResponse;
-        }
-    })());
-}});
+self.addEventListener('install', evt => {
+  console.log('[Service Worker] Install', evt);
+});
