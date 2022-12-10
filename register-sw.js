d1b720c2a0fec0ecd6a9c4956be42f3e9f282c9c
wensenfriendandextra <wensen.dong@gmail.com>
2022-12-10 21:44:01 +1100

    fix: debug element attribute

--- register-sw.js
+++ register-sw.js
@@ -1,4 +1,4 @@
-localStorage.debug = (document.getElementById('debug-elm')[document.getElementById('debug-attr')])?"on":"off";
+localStorage.debug = (document.getElementById('debug-elm')[document.getElementById('debug-attr').value])?"on":"off";
 document.getElementById('sw-btn').addEventListener(document.getElementById('sw-evt').value, () => {
     if ("serviceWorker" in navigator) {
       navigator.serviceWorker.register("sw.js");
