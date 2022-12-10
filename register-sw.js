58109f44aba11b6dc25de4d8351bed85a453675c
wensenfriendandextra <wensen.dong@gmail.com>
2022-12-11 09:09:26 +1100

    Debug messages

--- register-sw.js
+++ register-sw.js
@@ -1,13 +1,17 @@
 document.getElementById('sw-btn').addEventListener(
 document.getElementById('sw-evt').value, function() {
+console.log("Clicked")
 if ("serviceWorker" in navigator) {
 // \\ // \\ // \\ // \\ // \\ // \\
 
 
 
+console.log("Adding SW")
 if (self.BroadcastChannel) {
+  console.log("Broadcast Channel Exists")
   const LOG_CHANNEL = new BroadcastChannel('sw-logs');
   LOG_CHANNEL.onmessage = function(evt) {
+    console.log("Recieved message", evt.data)
     if (evt.data.constructor === Array) {
       // Logging func
       console.log(...evt.data);
@@ -21,11 +25,13 @@ if (self.BroadcastChannel) {
     let debugAttr = document.getElementById('debug-attr');
     let debugMode = debugElm[debugAttr.value];
     let debugModeVal = debugMode?"on":"off";
+    console.log('Debug mode is', debugModeVal)
     LOG_CHANNEL.postMessage(debugModeVal)
   };
 }
 
 navigator.serviceWorker.register("sw.js");
+console.log("Added SW")
 
 
 
