3a97d25571b4c8ff02c6fcb78775bb6ca5f35150
wensenfriendandextra <wensen.dong@gmail.com>
2022-12-11 07:58:08 +1100

    refactor: 2 space indentation

--- register-sw.js
+++ register-sw.js
@@ -1,9 +1,9 @@
 localStorage.debug = (document.getElementById('debug-elm')[document.getElementById('debug-attr').value])?"on":"off";
 (new BroadcastChannel('sw-logs')).onmessage = evt => {
-    console.log(...evt.data);
+  console.log(...evt.data);
 };
 document.getElementById('sw-btn').addEventListener(document.getElementById('sw-evt').value, () => {
-    if ("serviceWorker" in navigator) {
-        navigator.serviceWorker.register("sw.js");
-    }
+  if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.register("sw.js");
+  }
 });
--- sw.js
+++ sw.js
@@ -1,51 +1,51 @@
 const OFFLINE_CACHE = "offline-3";
 const DATA_CACHE = "data";
 const FILES = [
-    "/index.html",
-    "/app.webmanifest",
-    "/icon.svg",
-    "/main.css",
-    "/register-sw.js",
-    "/sw.js"
+  "/index.html",
+  "/app.webmanifest",
+  "/icon.svg",
+  "/main.css",
+  "/register-sw.js",
+  "/sw.js"
 ];
 const LOG_CHANNEL = new BroadcastChannel('sw-logs')
 
 function log(...args) {
-    if (localStorage.debug === "on") {
-        LOG_CHANNEL.postMessage(args)
-    }
+  if (localStorage.debug === "on") {
+    LOG_CHANNEL.postMessage(args)
+  }
 }
 
 function prettyLog(first, ...args) {
-    if (localStorage.debug === "on") {
-        args.unshift('[SW] ' + first)
-        LOG_CHANNEL.postMessage(args)
-    }
+  if (localStorage.debug === "on") {
+    args.unshift('[SW] ' + first)
+    LOG_CHANNEL.postMessage(args)
+  }
 }
 
 self.addEventListener("install", evt => {
-    log("Registered");
-    evt.waitUntil((async () => {
-        const cache = await caches.open(OFFLINE_CACHE);
-        log("Caching files");
-        await cache.addAll(FILES);
-    })());
+  log("Registered");
+  evt.waitUntil((async () => {
+    const cache = await caches.open(OFFLINE_CACHE);
+    log("Caching files");
+    await cache.addAll(FILES);
+  })());
 });
 
 self.addEventListener("fetch", evt => {
-    evt.respondWith((async () => {
-        try {
-            const preload = await evt.preloadResponse;
-            if (preload) {
-                return preload;
-            }
-            return (await fetch(evt.request));
-        }
-        catch (err) {
-            log("Fetch failed, returning cache");
-            const cache = await caches.open(CACHE_NAME);
-            const cachedResponse = await cache.match(OFFLINE_URL);
-            return cachedResponse;
-        }
-    })());
+  evt.respondWith((async () => {
+    try {
+      const preload = await evt.preloadResponse;
+      if (preload) {
+        return preload;
+      }
+      return (await fetch(evt.request));
+    }
+    catch (err) {
+      log("Fetch failed, returning cache");
+      const cache = await caches.open(CACHE_NAME);
+      const cachedResponse = await cache.match(OFFLINE_URL);
+      return cachedResponse;
+    }
+  })());
 });
